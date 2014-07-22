aa0e661fc30bdf74e408900123f863544b253153
dchao <dchao@caltech.edu>
2014-07-21 18:06:03 -0700

    Changed svm version to 3.18. Also added mtrand to utilities

--- utilities/svm.h
+++ utilities/svm.h
@@ -1,7 +1,7 @@
 #ifndef _LIBSVM_H
 #define _LIBSVM_H
 
-#define LIBSVM_VERSION 317
+#define LIBSVM_VERSION 318
 
 #ifdef __cplusplus
 extern "C" {
@@ -20,7 +20,6 @@ struct svm_problem
 	int l;
 	double *y;
 	struct svm_node **x;
-	double *W; /* instance weight */
 };
 
 enum { C_SVC, NU_SVC, ONE_CLASS, EPSILON_SVR, NU_SVR };	/* svm_type */
